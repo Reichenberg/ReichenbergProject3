f739038b57d1b224c331c3697119ea11a2e057b6
Reichenberg <chance.reichenberg@gmail.com>
2016-04-28 15:58:49 -0400

    Graph created for value selected

--- ReichenbergProject3/Scripts/MonthlySpending/MonthlySpendingMain.js
+++ ReichenbergProject3/Scripts/MonthlySpending/MonthlySpendingMain.js
@@ -11,10 +11,12 @@
                 var mappedItems = $.map(data, function (item) { return new Item(item); });
                 viewModel.Items(mappedItems);
                 ko.applyBindings(viewModel);
+                viewModel.UpdateChart();
             }
             else {
                 //push empty item to allow page to load with initial data
                 viewModel.Items.push(new Item());
+                //viewModel.SelectedItem(new Item());
                 ko.applyBindings(viewModel);
             }
         },
--- ReichenbergProject3/Scripts/MonthlySpending/MonthlySpendingViewModel.js
+++ ReichenbergProject3/Scripts/MonthlySpending/MonthlySpendingViewModel.js
@@ -13,6 +13,8 @@
     //Holds Item to be created
     self.NewItemName = ko.observable();
 
+
+
     self.CreateItem = function () {
         self.Items;
         $.ajax({
@@ -73,12 +75,48 @@
             success: function (data) {
                 var mappedItems = $.map(data, function (item) { return new Item(item); });
                 self.Items(mappedItems);
+                self.UpdateChart();
             },
             error: function (data) {
                 alert("Error getting items.");
             }
         });
     }
+
+    self.UpdateChart = function () {
+        var chart = new CanvasJS.Chart("spendingData", {
+            theme: "theme2",//theme1
+            title: {
+                text: self.SelectedItem().Name()
+            },
+            animationEnabled: true,   // change to true
+            data: [
+            {
+                // Change type to "bar", "area", "spline", "pie",etc.
+                type: "column",
+                dataPoints: [
+                    { label: "January", y: self.SelectedItem().January() },
+                    { label: "February", y: self.SelectedItem().February() },
+                    { label: "March", y: self.SelectedItem().March() },
+                    { label: "April", y: self.SelectedItem().April() },
+                    { label: "May", y: self.SelectedItem().May() },
+                    { label: "June", y: self.SelectedItem().June() },
+                    { label: "July", y: self.SelectedItem().July() },
+                    { label: "August", y: self.SelectedItem().August() },
+                    { label: "September", y: self.SelectedItem().September() },
+                    { label: "October", y: self.SelectedItem().October() },
+                      { label: "November", y: self.SelectedItem().November() },
+                    { label: "December", y: self.SelectedItem().December() }
+                ]
+            }
+            ]
+        });
+        chart.render();
+    }
+
+    self.SelectedItem.subscribe(function () {
+        self.UpdateChart();
+    }, null, "change");
 
 
 }
